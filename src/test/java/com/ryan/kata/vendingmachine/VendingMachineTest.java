c886e59ef452be96cdb0aeb0db3dbbe6c32f63f9
Ryan T <ryan.turinsky@gmail.com>
2017-07-10 21:47:57 -0400

    Quick refactor

--- src/test/java/com/ryan/kata/vendingmachine/VendingMachineTest.java
+++ src/test/java/com/ryan/kata/vendingmachine/VendingMachineTest.java
@@ -2,27 +2,32 @@ package com.ryan.kata.vendingmachine;
 
 import com.ryan.kata.coin.Coin;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Created by Styles on 7/3/17.
  */
 public class VendingMachineTest {
+    VendingMachine vendingMachine;
+
+    @Before
+    public void setUp() {
+        vendingMachine = new VendingMachine();
+    }
+
     @Test
     public void test_vending_machine_displays_insert_coin_with_no_coins_inserted() {
-        VendingMachine vendingMachine = new VendingMachine();
         Assert.assertEquals("INSERT COIN", vendingMachine.display());
     }
 
     @Test
     public void test_vending_machine_accepts_valid_coin() {
-        VendingMachine vendingMachine = new VendingMachine();
         Assert.assertTrue(vendingMachine.acceptCoin(Coin.QUARTER));
     }
 
     @Test
     public void test_vending_machine_rejects_invalid_coin_penny() {
-        VendingMachine vendingMachine = new VendingMachine();
         Assert.assertFalse(vendingMachine.acceptCoin(Coin.PENNY));
     }
 }
